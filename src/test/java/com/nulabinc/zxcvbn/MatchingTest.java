c861004cb10cadbf21c0ec01a9ced6e0006adb72
Maxim Mazin <Maxim.Mazin@jetbrains.com>
2016-01-25 18:04:05 +0100

    Use Parameterized in Measuretest

--- src/test/java/com/nulabinc/zxcvbn/MatchingTest.java
+++ src/test/java/com/nulabinc/zxcvbn/MatchingTest.java
@@ -2,6 +2,8 @@ package com.nulabinc.zxcvbn;
 
 import com.nulabinc.zxcvbn.matchers.*;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 import java.util.*;
 
@@ -9,48 +11,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class MatchingTest {
-    @Test
-    public void testMeasure() throws Exception {
-
-
-        Zxcvbn zxcvbn = new Zxcvbn();
-
-        for (String password : new String[]{
-                "qwER43@!",
-                "Tr0ub4dour&3",
-                "correcthorsebatterystaple",
-                "password",
-                "drowssap",
-                "passwordp",
-                "passwordadmin",
-                "p@$$word@dmin",
-                "19700101",
-                "20300101",
-                "aaaaaaaaa",
-                "123456789",
-                "abcdefghijklmnopqrstuvwxyz",
-                "qwertyuiop@[",
-                "zxcvbnm,./_",
-                "asdfghjkl;:]",
-                "pandapandapandapandapandapandapandapandapandaa",
-                "appleappleappleappleappleappleappleappleapplea",
-                "dncrbliehbvkehr734yf;ewhihwfph@houaegfueqpg30^r0urfvhej¥]e;l,ckvniwbgoidnci@oewhfoobojabouhqwou12482386fhoiwehe@o",
-                "apple orenge aabb ",
-                "eTq($%u-44c_j9NJB45a#2#JP7sH",
-                "IB7~EOw!51gug+7s#+%A9P1O/w8f",
-                "1v_f%7JvS8w!_t398+ON-CObI#v0",
-                "8lFmfc0!w)&iU9DM6~4_w)D)Y44J",
-                "&BZ09gjG!iKG&#M09s_1Gr41&o%i",
-                "T9Y-!ciS%XW9U5l/~aw9+4!5u8Ti",
-                "QMji&0uze5O#%+%2e_Y08E(R6L8p",
-                "6EG4y1nJASd!1~!//#6+Yhb1vW3d",
-                "8$q_5f2U3s6~W(S7iv)_8N%lJkOE",
-                "%nbd~$)2y/6hV6)2R9vYPpA49A~C",
-        }) {
-            Strength strength = zxcvbn.measure(password);
-            assertEquals("Unexpected error. Password is " + password, password, strength.getPassword());
-        }
-    }
 
     private static List<String[]> genpws(String pattern, List<String> prefixes, List<String> suffixes) {
         List<String> pres = prefixes.subList(0, prefixes.size());
