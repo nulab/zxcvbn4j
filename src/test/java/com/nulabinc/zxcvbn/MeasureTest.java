3e7018a3ad6d0e9a8e26aef83f8bcfb4b9c9734c
yuichi.watanabe <yuichi.watanabe.ja@gmail.com>
2016-09-27 20:23:48 +0900

    add check infinite

--- src/test/java/com/nulabinc/zxcvbn/MeasureTest.java
+++ src/test/java/com/nulabinc/zxcvbn/MeasureTest.java
@@ -56,6 +56,7 @@ public class MeasureTest {
                 {"6EG4y1nJASd!1~!//#6+Yhb1vW3d"},
                 {"8$q_5f2U3s6~W(S7iv)_8N%lJkOE"},
                 {"%nbd~$)2y/6hV6)2R9vYPpA49A~C"},
+                {"Rh&pW%EXT=/Z1lzouG.wU_+2MT+FG4sm+&jqN?L25jDtjW3EQuppfvD_30Vo3K=SX4=z3-U2gVf7A0oSM5oWegRa_sV$-GLI3LzCo&@!h@$v#OkoN#@-eS8Y&W$pGmmVXc#XHAv?n$M+_wQx1FAB_*iaZE1_9ZV.cwn-d@+90B8z0bVOKc63lV9QntW0kryN7Y#rjv@0+Bd8hc-3WW_Yn%z5/DE?R*UeiKgR#$/F8kA9I!Ib*GDa.x0T7UWCCxDV&ithebyz$=7vW6TdmlmL%WZxmA7K%*Rg1035UO%WOTIgiMs4AjpmL1"}
         });
     }
 }
