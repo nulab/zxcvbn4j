8b71fe0dc51f8c1499d24d5337694845fce2c9c6
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-08-13 07:56:39 +0900

    refactor: refactoring matchers.RegexMatcher

--- src/main/java/com/nulabinc/zxcvbn/matchers/RegexMatcher.java
+++ src/main/java/com/nulabinc/zxcvbn/matchers/RegexMatcher.java
@@ -10,10 +10,10 @@ import java.util.regex.*;
 
 public class RegexMatcher extends BaseMatcher {
 
-  private static final Map<String, String> REGEXEN = new HashMap<>();
+  private static final Map<String, Pattern> PATTERNS = new HashMap<>();
 
   static {
-    REGEXEN.put("recent_year", "19\\d\\d|200\\d|201\\d|202\\d");
+    PATTERNS.put("recent_year", Pattern.compile("19\\d\\d|200\\d|201\\d|202\\d"));
   }
 
   public RegexMatcher(final Context context) {
@@ -23,14 +23,14 @@ public class RegexMatcher extends BaseMatcher {
   @Override
   public List<Match> execute(CharSequence password) {
     List<Match> matches = new ArrayList<>();
-    for (Map.Entry<String, String> regexenRef : REGEXEN.entrySet()) {
-      String name = regexenRef.getKey();
-      java.util.regex.Matcher rxMatch = Pattern.compile(regexenRef.getValue()).matcher(password);
-      while (rxMatch.find()) {
-        CharSequence token = new WipeableString(rxMatch.group());
+    for (Map.Entry<String, Pattern> patternRef : PATTERNS.entrySet()) {
+      String name = patternRef.getKey();
+      java.util.regex.Matcher matcher = patternRef.getValue().matcher(password);
+      while (matcher.find()) {
+        CharSequence token = new WipeableString(matcher.group());
         matches.add(
             MatchFactory.createRegexMatch(
-                rxMatch.start(), rxMatch.start() + token.length() - 1, token, name, rxMatch));
+                matcher.start(), matcher.start() + token.length() - 1, token, name, matcher));
       }
     }
     return this.sorted(matches);
