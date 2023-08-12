d078fe2ea7db47f8e194afdbe82ed761861bd0a5
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-08-13 06:27:02 +0900

    style: format code and optimize imports in matchers.DictionaryMatcher

--- src/main/java/com/nulabinc/zxcvbn/matchers/DictionaryMatcher.java
+++ src/main/java/com/nulabinc/zxcvbn/matchers/DictionaryMatcher.java
@@ -2,7 +2,6 @@ package com.nulabinc.zxcvbn.matchers;
 
 import com.nulabinc.zxcvbn.Context;
 import com.nulabinc.zxcvbn.WipeableString;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,37 +9,39 @@ import java.util.Map;
 
 public class DictionaryMatcher extends BaseMatcher {
 
-    private final Map<String, Map<String, Integer>> rankedDictionaries;
+  private final Map<String, Map<String, Integer>> rankedDictionaries;
 
-    public DictionaryMatcher(Context context, Map<String, Map<String, Integer>> rankedDictionaries) {
-        super(context);
-        if (rankedDictionaries == null) {
-            this.rankedDictionaries = new HashMap<>();
-        } else {
-            this.rankedDictionaries = rankedDictionaries;
-        }
+  public DictionaryMatcher(Context context, Map<String, Map<String, Integer>> rankedDictionaries) {
+    super(context);
+    if (rankedDictionaries == null) {
+      this.rankedDictionaries = new HashMap<>();
+    } else {
+      this.rankedDictionaries = rankedDictionaries;
     }
+  }
 
-    @Override
-    public List<Match> execute(CharSequence password) {
-        List<Match> matches = new ArrayList<>();
-        int len = password.length();
-        WipeableString passwordLower = WipeableString.lowerCase(password);
-        for (Map.Entry<String, Map<String, Integer>> rankedDictionaryRef : this.rankedDictionaries.entrySet()) {
-            String dictionaryName = rankedDictionaryRef.getKey();
-            Map<String, Integer> rankedDict = rankedDictionaryRef.getValue();
-            for (int i = 0; i < len; i++) {
-                for (int j = i; j < len; j++) {
-                    CharSequence word = passwordLower.subSequence(i, j + 1).toString();
-                    if (rankedDict.containsKey(word)) {
-                        int rank = rankedDict.get(word);
-                        WipeableString token = WipeableString.copy(password, i, j + 1);
-                        matches.add(MatchFactory.createDictionaryMatch(i, j, token, word, rank, dictionaryName));
-                    }
-                }
-            }
+  @Override
+  public List<Match> execute(CharSequence password) {
+    List<Match> matches = new ArrayList<>();
+    int len = password.length();
+    WipeableString passwordLower = WipeableString.lowerCase(password);
+    for (Map.Entry<String, Map<String, Integer>> rankedDictionaryRef :
+        this.rankedDictionaries.entrySet()) {
+      String dictionaryName = rankedDictionaryRef.getKey();
+      Map<String, Integer> rankedDict = rankedDictionaryRef.getValue();
+      for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+          CharSequence word = passwordLower.subSequence(i, j + 1).toString();
+          if (rankedDict.containsKey(word)) {
+            int rank = rankedDict.get(word);
+            WipeableString token = WipeableString.copy(password, i, j + 1);
+            matches.add(
+                MatchFactory.createDictionaryMatch(i, j, token, word, rank, dictionaryName));
+          }
         }
-        passwordLower.wipe();
-        return this.sorted(matches);
+      }
     }
+    passwordLower.wipe();
+    return this.sorted(matches);
+  }
 }
