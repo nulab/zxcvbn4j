9236d2f85c0d35c7a3ebbede7cf5d02945e78be2
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-08-13 07:42:06 +0900

    style: format code and optimize imports in matchers.RegexMatcher

--- src/main/java/com/nulabinc/zxcvbn/matchers/RegexMatcher.java
+++ src/main/java/com/nulabinc/zxcvbn/matchers/RegexMatcher.java
@@ -2,7 +2,6 @@ package com.nulabinc.zxcvbn.matchers;
 
 import com.nulabinc.zxcvbn.Context;
 import com.nulabinc.zxcvbn.WipeableString;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,31 +10,29 @@ import java.util.regex.*;
 
 public class RegexMatcher extends BaseMatcher {
 
-    private static final Map<String, String> REGEXEN = new HashMap<>();
-    static {
-        REGEXEN.put("recent_year", "19\\d\\d|200\\d|201\\d|202\\d");
-    }
+  private static final Map<String, String> REGEXEN = new HashMap<>();
 
-    public RegexMatcher(final Context context) {
-        super(context);
-    }
+  static {
+    REGEXEN.put("recent_year", "19\\d\\d|200\\d|201\\d|202\\d");
+  }
 
-    @Override
-    public List<Match> execute(CharSequence password) {
-        List<Match> matches = new ArrayList<>();
-        for(Map.Entry<String, String> regexenRef: REGEXEN.entrySet()) {
-            String name = regexenRef.getKey();
-            java.util.regex.Matcher rxMatch = Pattern.compile(regexenRef.getValue()).matcher(password);
-            while(rxMatch.find()){
-                CharSequence token = new WipeableString(rxMatch.group());
-                matches.add(MatchFactory.createRegexMatch(
-                        rxMatch.start(),
-                        rxMatch.start() + token.length() - 1,
-                        token,
-                        name,
-                        rxMatch));
-            }
-        }
-        return this.sorted(matches);
+  public RegexMatcher(final Context context) {
+    super(context);
+  }
+
+  @Override
+  public List<Match> execute(CharSequence password) {
+    List<Match> matches = new ArrayList<>();
+    for (Map.Entry<String, String> regexenRef : REGEXEN.entrySet()) {
+      String name = regexenRef.getKey();
+      java.util.regex.Matcher rxMatch = Pattern.compile(regexenRef.getValue()).matcher(password);
+      while (rxMatch.find()) {
+        CharSequence token = new WipeableString(rxMatch.group());
+        matches.add(
+            MatchFactory.createRegexMatch(
+                rxMatch.start(), rxMatch.start() + token.length() - 1, token, name, rxMatch));
+      }
     }
+    return this.sorted(matches);
+  }
 }
