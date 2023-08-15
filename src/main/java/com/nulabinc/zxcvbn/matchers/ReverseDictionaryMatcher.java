0a84c7d43971e8e4b362481cfd7a8d8cb169b151
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-08-15 12:43:13 +0900

    style: format code and optimize imports in matchers.ReverseDictionaryMatcher

--- src/main/java/com/nulabinc/zxcvbn/matchers/ReverseDictionaryMatcher.java
+++ src/main/java/com/nulabinc/zxcvbn/matchers/ReverseDictionaryMatcher.java
@@ -2,7 +2,6 @@ package com.nulabinc.zxcvbn.matchers;
 
 import com.nulabinc.zxcvbn.Context;
 import com.nulabinc.zxcvbn.WipeableString;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,30 +9,34 @@ import java.util.Map;
 
 public class ReverseDictionaryMatcher extends BaseMatcher {
 
-    private final Map<String, Map<String, Integer>> rankedDictionaries;
+  private final Map<String, Map<String, Integer>> rankedDictionaries;
 
-    public ReverseDictionaryMatcher(Context context, Map<String, Map<String, Integer>> rankedDictionaries) {
-        super(context);
-        if (rankedDictionaries == null) {
-            this.rankedDictionaries = new HashMap<>();
-        } else {
-            this.rankedDictionaries = rankedDictionaries;
-        }
+  public ReverseDictionaryMatcher(
+      Context context, Map<String, Map<String, Integer>> rankedDictionaries) {
+    super(context);
+    if (rankedDictionaries == null) {
+      this.rankedDictionaries = new HashMap<>();
+    } else {
+      this.rankedDictionaries = rankedDictionaries;
     }
+  }
 
-    @Override
-    public List<Match> execute(CharSequence password) {
-        CharSequence reversedPassword =  WipeableString.reversed(password);
-        List<Match> matches = new ArrayList<>();
-        for (Match match: new DictionaryMatcher(this.getContext(), this.rankedDictionaries).execute(reversedPassword)) {
-            matches.add(MatchFactory.createReversedDictionaryMatch(
-                    password.length() - 1 - match.j,
-                    password.length() - 1 - match.i,
-                    WipeableString.reversed(match.token),
-                    match.matchedWord,
-                    match.rank,
-                    match.dictionaryName));
-        }
-        return this.sorted(matches);
+  @Override
+  public List<Match> execute(CharSequence password) {
+    CharSequence reversedPassword = WipeableString.reversed(password);
+    List<Match> matches = new ArrayList<>();
+    for (Match match :
+        new DictionaryMatcher(this.getContext(), this.rankedDictionaries)
+            .execute(reversedPassword)) {
+      matches.add(
+          MatchFactory.createReversedDictionaryMatch(
+              password.length() - 1 - match.j,
+              password.length() - 1 - match.i,
+              WipeableString.reversed(match.token),
+              match.matchedWord,
+              match.rank,
+              match.dictionaryName));
     }
+    return this.sorted(matches);
+  }
 }
