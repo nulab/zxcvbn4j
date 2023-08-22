02f369a0f24d261234adad91d75267991e69cc2a
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-08-22 12:26:16 +0900

    refactor: add brackets to the if statement in BaseGuess

--- src/main/java/com/nulabinc/zxcvbn/guesses/BaseGuess.java
+++ src/main/java/com/nulabinc/zxcvbn/guesses/BaseGuess.java
@@ -17,8 +17,12 @@ public abstract class BaseGuess implements Guess {
 
   protected static int nCk(int n, int k) {
     // http://blog.plover.com/math/choose.html
-    if (k > n) return 0;
-    if (k == 0) return 1;
+    if (k > n) {
+      return 0;
+    }
+    if (k == 0) {
+      return 1;
+    }
     int r = 1;
     for (int d = 1; d <= k; d++) {
       r *= n;
