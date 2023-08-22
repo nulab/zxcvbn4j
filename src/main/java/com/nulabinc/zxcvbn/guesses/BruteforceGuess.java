4a9f72c0a797e18e5d1a334ee728c075a8d6ba19
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-08-22 12:30:48 +0900

    refactor: split the logic inside the exec method of BruteforceGuess into two new private methods

--- src/main/java/com/nulabinc/zxcvbn/guesses/BruteforceGuess.java
+++ src/main/java/com/nulabinc/zxcvbn/guesses/BruteforceGuess.java
@@ -11,14 +11,19 @@ public class BruteforceGuess extends BaseGuess {
 
   @Override
   public double exec(Match match) {
-    double guesses = Math.pow(BRUTEFORCE_CARDINALITY, match.tokenLength());
-    if (Double.isInfinite(guesses)) {
-      guesses = Double.MAX_VALUE;
-    }
-    double minGuesses =
-        match.tokenLength() == 1
-            ? MIN_SUBMATCH_GUESSES_SINGLE_CHAR + 1
-            : MIN_SUBMATCH_GUESSES_MULTI_CHAR + 1;
+    double guesses = calculateBruteforceGuesses(match.tokenLength());
+    double minGuesses = calculateMinGuesses(match.tokenLength());
     return Math.max(guesses, minGuesses);
+  }
+
+  private double calculateBruteforceGuesses(int tokenLength) {
+    double guesses = Math.pow(BRUTEFORCE_CARDINALITY, tokenLength);
+    return Double.isInfinite(guesses) ? Double.MAX_VALUE : guesses;
+  }
+
+  private double calculateMinGuesses(int tokenLength) {
+    return tokenLength == 1
+        ? MIN_SUBMATCH_GUESSES_SINGLE_CHAR + 1
+        : MIN_SUBMATCH_GUESSES_MULTI_CHAR + 1;
   }
 }
