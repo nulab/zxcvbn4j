d4f817b3ddd7e17b15586d22e50a6297a19065fb
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-08-23 18:39:00 +0900

    refactor: replaced switch with map lookup, extracted guess initialization, and reduced nesting with early returns in EstimateGuess

--- src/main/java/com/nulabinc/zxcvbn/guesses/EstimateGuess.java
+++ src/main/java/com/nulabinc/zxcvbn/guesses/EstimateGuess.java
@@ -2,21 +2,35 @@ package com.nulabinc.zxcvbn.guesses;
 
 import com.nulabinc.zxcvbn.Context;
 import com.nulabinc.zxcvbn.Guess;
+import com.nulabinc.zxcvbn.Pattern;
 import com.nulabinc.zxcvbn.Scoring;
 import com.nulabinc.zxcvbn.matchers.Match;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EstimateGuess extends BaseGuess {
 
   private final CharSequence password;
+  private final Map<Pattern, Guess> patternGuessMap = new HashMap<>();
 
   public EstimateGuess(Context context, CharSequence password) {
     super(context);
     this.password = password;
+    patternGuessMap.put(Pattern.Bruteforce, new BruteforceGuess(context));
+    patternGuessMap.put(Pattern.Dictionary, new DictionaryGuess(context));
+    patternGuessMap.put(Pattern.Spatial, new SpatialGuess(context));
+    patternGuessMap.put(Pattern.Repeat, new RepeatGuess(context));
+    patternGuessMap.put(Pattern.Sequence, new SequenceGuess(context));
+    patternGuessMap.put(Pattern.Regex, new RegexGuess(context));
+    patternGuessMap.put(Pattern.Date, new DateGuess(context));
   }
 
   @Override
   public double exec(Match match) {
-    if (match.guesses != null) return match.guesses;
+    if (match.guesses != null) {
+      return match.guesses;
+    }
+
     int minGuesses = 1;
     if (match.tokenLength() < password.length()) {
       minGuesses =
@@ -24,34 +38,10 @@ public class EstimateGuess extends BaseGuess {
               ? MIN_SUBMATCH_GUESSES_SINGLE_CHAR
               : MIN_SUBMATCH_GUESSES_MULTI_CHAR;
     }
-    final Guess guess;
-    switch (match.pattern) {
-      case Bruteforce:
-        guess = new BruteforceGuess(this.getContext());
-        break;
-      case Dictionary:
-        guess = new DictionaryGuess(this.getContext());
-        break;
-      case Spatial:
-        guess = new SpatialGuess(this.getContext());
-        break;
-      case Repeat:
-        guess = new RepeatGuess(this.getContext());
-        break;
-      case Sequence:
-        guess = new SequenceGuess(this.getContext());
-        break;
-      case Regex:
-        guess = new RegexGuess(this.getContext());
-        break;
-      case Date:
-        guess = new DateGuess(this.getContext());
-        break;
-      default:
-        guess = null;
-        break;
-    }
+
+    Guess guess = patternGuessMap.get(match.pattern);
     double guesses = guess != null ? guess.exec(match) : 0;
+
     match.guesses = Math.max(guesses, minGuesses);
     match.guessesLog10 = Scoring.log10(match.guesses);
     return match.guesses;
