c3408e9c755e833030e7ff11dd0fe558077ce5ba
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-08-29 09:48:53 +0900

    fix: fix the code smell warnings in Pattern

--- src/main/java/com/nulabinc/zxcvbn/Pattern.java
+++ src/main/java/com/nulabinc/zxcvbn/Pattern.java
@@ -1,12 +1,19 @@
 package com.nulabinc.zxcvbn;
 
 public enum Pattern {
+  @SuppressWarnings("squid:S115")
   Bruteforce("bruteforce"),
+  @SuppressWarnings("squid:S115")
   Dictionary("dictionary"),
+  @SuppressWarnings("squid:S115")
   Spatial("spatial"),
+  @SuppressWarnings("squid:S115")
   Repeat("repeat"),
+  @SuppressWarnings("squid:S115")
   Sequence("sequence"),
+  @SuppressWarnings("squid:S115")
   Regex("regex"),
+  @SuppressWarnings("squid:S115")
   Date("date");
 
   private final String value;
