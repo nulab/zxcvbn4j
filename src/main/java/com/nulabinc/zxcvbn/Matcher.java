277331b036455494a849370682bdee9329dcaf2b
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-09-03 17:42:00 +0900

    fix: checkstyle warnings (Summary javadoc is missin)

--- src/main/java/com/nulabinc/zxcvbn/Matcher.java
+++ src/main/java/com/nulabinc/zxcvbn/Matcher.java
@@ -4,6 +4,11 @@ import com.nulabinc.zxcvbn.matchers.Match;
 import java.util.List;
 
 /**
+ * Represents a matcher responsible for identifying patterns within passwords.
+ *
+ * <p>Implementations of this interface provide specific matching strategies to detect various
+ * patterns such as dictionary words, sequences, and spatial patterns.
+ *
  * @deprecated This interface is deprecated. Use {@link com.nulabinc.zxcvbn.matchers.Matcher}
  *     instead.
  */
